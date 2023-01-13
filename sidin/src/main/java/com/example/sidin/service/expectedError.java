d83eb035c7e0a8dc2faeff358562a33721f359cd
Muhammad Thalenta Dirgantara Deha <92012703+thalentadeha@users.noreply.github.com>
2023-01-13 15:21:54 +0700

    pembersihan

--- sidin/src/main/java/com/example/sidin/service/expectedError.java
+++
@@ -1,9 +0,0 @@
-package com.example.sidin.service;
-import org.springframework.ui.Model;
-
-public class expectedError {
-    static <T,V> void giveError(T param1,Model model)
-    {
-        model.addAttribute("param1", param1);
-    }
-}
